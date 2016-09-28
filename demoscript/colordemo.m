cb19a50e48e089a953f4b27e9506044a3c914638
Andy K. Massimino <massimino@gatech.edu>
2016-09-28 11:14:06 -0400

    Fixed color assignment, improved colordemo
    - Fixed warnings and errors assigned the same color
    - Fixed colordemo to force refresh of command window
        along with other improvements
    - Updated screenshot
    
    
    Former-commit-id: 63daf15ffdc6fe60ecc643a5b2be1521b8714d29 [formerly ef837ab96f964dffa4a6b0e7138af91f15102a90]
    Former-commit-id: 1f21f51031037d8b43064dceb88d376e984adfe9

--- demoscript/colordemo.m
+++ demoscript/colordemo.m
@@ -1,24 +1,41 @@
-cd('..');
+function colordemo
 
-if system('command -v wmctrl >/dev/null 2>&1')
-    warning('resize requires wmctrl');
-else
-    system('wmctrl -r "MATLAB R" -e 0,50,50,650,500');
-end
 if system('command -v scrot >/dev/null 2>&1')
     error('requires scrot');
 end
+if system('command -v wmctrl >/dev/null 2>&1')
+    warning('requires wmctrl');
+end
 
-[~,~] = mkdir('screenshots');
-fid = fopen('Home.md', 'w');
+system('wmctrl -r "MATLAB R" -e 0,50,50,650,500');
+
+[olddir, oldtl, fid] = deal([]);
+try
+    olddir = cd('..');
+    [~,~] = mkdir('screenshots');
+
+    fid = fopen('Home.md', 'w');
+
+    oldtl = com.mathworks.services.Prefs.getIntegerPref('EditorRightTextLineLimit');
+    com.mathworks.services.Prefs.setIntegerPref('EditorRightTextLineLimit', 40);
+
+    cleanup = onCleanup(@() colordemo_cleanup(olddir, oldtl, fid) );
+catch
+    colordemo_cleanup(olddir, oldtl, fid);
+end
 
 files = dir('colors/*.m');
 for ii = 1:length(files)
     file = files(ii).name;
     scheme = regexp(file, '(base16-)([\w-]+).m', 'tokens');
-    for variant = 0:1
+    for variant = 1:-1:0
         apply_colors(scheme{1}{2}, 1, variant);
-        pause(1);
+
+        % force unfocus-focus
+        figure(1); drawnow;
+        system('wmctrl -R "MATLAB R"');
+        pause(0.5);
+
         if variant
             img = [scheme{1}{:} '.png'];
             alt = [scheme{1}{2} ''];
@@ -32,6 +49,16 @@ for ii = 1:length(files)
     end
 end
 
-fclose(fid);
-cd('demoscript');
+function r = colordemo_cleanup(olddir, oldtl, fid)
 
+if ~isempty(fid)
+    fclose(fid);
+end
+if ~isempty(olddir)
+    cd(olddir);
+end
+if ~isempty(oldtl)
+    com.mathworks.services.Prefs.setIntegerPref('EditorRightTextLineLimit', oldtl);
+end
+
+r = 0;
